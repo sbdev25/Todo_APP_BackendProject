9b0371c03858916c8d1d67e67ae2114fd34898ec
soheib <bouafiasoheib918@gmail.com>
2026-03-13 00:46:23 +0100

    urls.py app is done

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('todos/' , views.getTodos),
-    
-    
+    path('todos/add/' , views.addTodos),
+    path('todos/edit/<int:pk>' , views.updateTodo),
+    path('todos/delete/<int:pk>',views.deleteTodo),
 ]
