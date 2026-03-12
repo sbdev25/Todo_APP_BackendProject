0fdc4d33f7c90df94ae71ffb29ed84c22d93328c
soheib <bouafiasoheib918@gmail.com>
2026-03-12 23:21:37 +0100

    todoClass in modelsp.y is done and migrated

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model) : 
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User , on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=50)
+    description=models.TextField(max_length=500)
+    isCompleted=models.BooleanField(default=False) 
+    createdAt = models.DateTimeField(auto_now_add=True)
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+    
+    
+class MyTokenObtainPairView(TokenObtainPairView): 
+    serializer = MyTokenObtainPairSerializer
+
